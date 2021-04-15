218809b609c932ed31bc256718f50d1caf6cf39d
Brandon Smith <mail@brandonsmith.ninja>
2021-04-15 00:20:30 -0500

    Added main() function call to compiled script, and improved timing log output

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,10 @@ const entry = process.argv[2];
 const output = process.argv[3];
 
 fs.readFile(entry).then(async code => {
-    const start = Date.now();
+    const startParse = Date.now();
     const parsed = parse(code.toString());
-    console.log(`Parsed in ${Date.now() - start}ms`)
+    const endParse = Date.now();
+    
     console.log(JSON.stringify(parsed, null, 2))
     const compiled = compile(parsed)
     console.log(compiled)
@@ -24,10 +25,18 @@ fs.readFile(entry).then(async code => {
 
 Object.entries(this["bagel-lib"]).forEach(([key, value]) => this[key] = value);
 
-${compiled}`;
+${compiled}
 
-    console.log(typecheckFile(parsed))
+main();`;
+
+    const startTypecheck = Date.now();
+    const types = typecheckFile(parsed);
+    const endTypecheck = Date.now();
+    console.log(types);
     fs.writeFile(output, compiledWithLib);
     // console.log(eval(compiled))
     
+    console.log();
+    console.log(`Parsed in ${endParse - startParse}ms`)
+    console.log(`Typechecked in ${endTypecheck - startTypecheck}ms`)
 });
