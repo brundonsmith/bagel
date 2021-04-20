74d728d4533cdbf88d87eade6b6885f027b8adce
Brandon Smith <mail@brandonsmith.ninja>
2021-04-19 21:10:36 -0500

    Allow running just one test at a time

--- tests/src/testing-utils.ts
+++ tests/src/testing-utils.ts
@@ -1,11 +1,13 @@
 
 export function test(fn: () => string|void) {
-    const result = fn();
-
-    if (result != null) {
-        const description = typeof result === "string" ? `: "${result}"` : ``;
-        console.error(`❌ ${fn.name}` + description)
-    } else {
-        console.log(`✅ ${fn.name}`)
+    if (!process.argv[2] || process.argv[2] === fn.name) {
+        const result = fn();
+    
+        if (result != null) {
+            const description = typeof result === "string" ? `: "${result}"` : ``;
+            console.error(`❌ ${fn.name}` + description)
+        } else {
+            console.log(`✅ ${fn.name}`)
+        }
     }
 }
