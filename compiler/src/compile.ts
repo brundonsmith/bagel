98edc065e3a3bb370488f2507d018d04a6009f70
Brandon Smith <mail@brandonsmith.ninja>
2021-04-22 19:51:46 -0500

    Got rid of __locals for now, and fixed function and proc declarations to support partial-application

--- compiler/src/compile.ts
+++ compiler/src/compile.ts
@@ -2,9 +2,11 @@ import { AST, Declaration, declarationName, Expression, Func, LocalIdentifier, P
 
 export function compile(declarations: Declaration[]): string {
     return `
-    ${declarations.map(compileOne).join("\n\n")}
+Object.entries(window["bagel-lib"]).forEach(([key, value]) => window[key] = value)
 
-    main({...window["bagel-lib"],${declarations.map(declarationName).map(ident => ident.name).join(",")}});`;
+${declarations.map(compileOne).join("\n\n")}
+
+main();`;
 }
 
 function compileOne(ast: AST): string {
@@ -16,19 +18,19 @@ function compileOne(ast: AST): string {
         case "proc": return compileProc(ast);
         case "let-declaration": return `${compileOne(ast.name)} = ${compileOne(ast.value)}`;
         case "assignment": return `${compileOne(ast.target)} = ${compileOne(ast.value)}`;
-        case "proc-call": return `${compileOne(ast.proc)}${ast.args.map(arg => `(${LOCALS_OBJ}, ${compileOne(arg)})`).join("")}`;
+        case "proc-call": return `${compileOne(ast.proc)}${ast.args.map(arg => `(${compileOne(arg)})`).join("")}`;
         case "if-else-statement": return `if(${compileOne(ast.ifCondition)}) { ${ast.ifResult.map(compileOne).join(" ")} }` + (ast.elseResult != null ? ` else { ${ast.elseResult.map(compileOne).join(" ")} }` : ``);
         case "for-loop": return `for (const ${compileOne(ast.itemIdentifier)} of ${compileOne(ast.iterator)}) { ${ast.body.map(compileOne).join(" ")} }`;
         case "while-loop": return `while (${compileOne(ast.condition)}) { ${ast.body.map(compileOne).join(" ")} }`;
         case "func": return compileFunc(ast);
-        case "funcall": return `${compileOne(ast.func)}${ast.args.map(arg => `(${LOCALS_OBJ}, ${compileOne(arg)})`).join("")}`;
+        case "funcall": return `${compileOne(ast.func)}${ast.args.map(arg => `(${compileOne(arg)})`).join("")}`;
         case "pipe": return compilePipe(ast.expressions, ast.expressions.length - 1);
         case "binary-operator": return `${compileOne(ast.left)} ${ast.operator} ${compileOne(ast.right)}`;
         case "if-else-expression": return `(${compileOne(ast.ifCondition)}) ? (${compileOne(ast.ifResult)}) : (${ast.elseResult == null ? NIL : compileOne(ast.elseResult)})`;
         case "range": return `range(${ast.start})(${ast.end})`;
         case "parenthesized-expression": return `(${compileOne(ast.inner)})`;
         case "property-accessor": return `${compileOne(ast.base)}.${ast.properties.map(compileOne).join(".")}`;
-        case "local-identifier": return `${LOCALS_OBJ}.${ast.name}`;
+        case "local-identifier": return `${ast.name}`;
         case "plain-identifier": return ast.name;
         case "object-literal":  return `{ ${ast.entries.map(([ key, value ]) => `${compileOne(key)}: ${compileOne(value)}`).join(", ")} }`;
         case "array-literal":   return `[${ast.entries.map(compileOne).join(", ")}]`;
@@ -40,7 +42,7 @@ function compileOne(ast: AST): string {
         case "boolean-literal": return JSON.stringify(ast.value);
         case "nil-literal": return NIL;
         case "javascript-escape": return ast.js;
-        case "reaction": return `disposers.push(__locals.crowdx.reaction(() => ${compileOne(ast.data)}(${LOCALS_OBJ}), (data) => ${compileOne(ast.effect)}(${LOCALS_OBJ}, data)))`;
+        case "reaction": return `disposers.push(crowdx.reaction(() => ${compileOne(ast.data)}, (data) => ${compileOne(ast.effect)}(data)))`;
         case "indexer": return `${compileOne(ast.base)}[${compileOne(ast.indexer)}]`;
     }
 
@@ -54,22 +56,22 @@ export const LOCALS_OBJ = "__locals";
 //.map(name => `${name}:{value:${name}}`)
 // let ${LOCALS_OBJ} = __locals.crowdx.observable(Object.create(${LOCALS_OBJ}, {${proc.argNames.map(compileOne).map(arg => `${arg}: {value: ${arg}}`).join(", ")}}));
 function compileProc(proc: Proc): string {
-    return `function ${proc.name == null ? '' : proc.name.name}(__parent_locals, ${proc.argNames.map(compileOne)}) {
-    const ${LOCALS_OBJ} = __parent_locals.crowdx.observable({...__parent_locals,${proc.argNames.map(compileOne).join(",")}});
+    return `function ${proc.name == null ? '' : proc.name.name}(${proc.argNames[0] != null ? compileOne(proc.argNames[0]) : ''}) {
+    ${proc.argNames.length > 1 ? ` return (${proc.argNames.map((arg, index) => index === 0 ? '' : `(${compileOne(arg)}) => `).join("")}{` : ''}
     const disposers = [];
 
     ${proc.body.map(compileOne).join("; ")}
 
-    // disposers.forEach(__locals.crowdx.dispose);
+    // disposers.forEach(crowdx.dispose);
+    ${proc.argNames.length > 1 ? `});` : ''}
 }`;
 }
 // TODO: dispose of reactions somehow... at some point...
 
 // TODO: Don't pass __parent_locals to top-level declared functions/procs
 function compileFunc(func: Func): string {
-    return `function ${func.name == null ? '' : func.name.name}(__parent_locals, ${func.argNames.map(compileOne)}) {
-        ${func.argNames.length > 0 ? `const ${LOCALS_OBJ} = {...__parent_locals,${func.argNames.map(compileOne).join(",")}};` : ``}
-        return ${compileOne(func.body)};
+    return `function ${func.name == null ? '' : func.name.name}(${func.argNames[0] != null ? compileOne(func.argNames[0]) : ''}) {
+        return ${func.argNames.map((arg, index) => index === 0 ? '' : `(${compileOne(arg)}) => `).join("")}${compileOne(func.body)};
     }`;
 }
 
