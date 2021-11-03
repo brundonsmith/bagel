2ff3dc01cdd95bb226762c13aa0f06b9b7bbf686
Brandon Smith <mail@brandonsmith.ninja>
2021-11-02 21:47:52 -0500

    Re-orient lib/time around Clock and Instant classes

--- lib/src/time.ts
+++ lib/src/time.ts
@@ -1,14 +1,7 @@
 
 import { observable, computed } from "mobx"
-import { Temporal } from '@js-temporal/polyfill'
 
-const time: { now: BigInt } = observable({ now: BigInt(Date.now()) * 1000000n })
-const now = computed(() =>
-    new Temporal.ZonedDateTime(
-        time.now, // epoch nanoseconds
-        Temporal.TimeZone.from('America/Chicago'), // timezone
-        Temporal.Calendar.from('iso8601') // default calendar
-    ))
+export const time: { now: BigInt } = observable({ now: BigInt(Date.now()) * 1000000n })
 
 function updateTime() {
     time.now = BigInt(Date.now()) * 1000000n
@@ -16,26 +9,9 @@ function updateTime() {
 }
 
 let updateTimeStarted = false;
-function updateTimeIfNeeded() {
+export function updateTimeIfNeeded() {
     if (!updateTimeStarted) {
         updateTimeStarted = true;
         updateTime();
     }
-}
-
-export const millis = () => {
-    updateTimeIfNeeded()
-    return now.get().millisecond
-}
-export const seconds = () => {
-    updateTimeIfNeeded()
-    return now.get().second
-}
-export const minutes = () => {
-    updateTimeIfNeeded()
-    return now.get().minute
-}
-export const hours = () => {
-    updateTimeIfNeeded()
-    return now.get().hour
 }
