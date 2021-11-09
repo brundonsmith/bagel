e8fab490fa6192fa332f358835efcfb34534e805
Brandon Smith <mail@brandons.me>
2021-11-09 02:27:13 +0000

    Pin MobX to specific versions that work in Deno

--- compiler/mobx.ts
+++ compiler/mobx.ts
@@ -1,10 +1,10 @@
 
-import * as mobx from "https://jspm.dev/mobx"
-import * as mobx_utils from "https://jspm.dev/mobx-utils"
+import * as mobx from "https://jspm.dev/npm:mobx@6.3.3"
+import * as mobx_utils from "https://jspm.dev/npm:mobx-utils@6.0.4"
 
-export const configure: (options: Record<string, unknown>) => void = mobx.configure
-export const observable: <T>(x: T) => T = mobx.observable
-export const autorun: (fn: () => void) => void = mobx.autorun
-export const computed: <T>(fn: () => T) => { get(): T } = mobx.computed
+export const configure: (options: Record<string, unknown>) => void = mobx.configure as any
+export const observable: <T>(x: T) => T = mobx.observable as any
+export const autorun: (fn: () => void) => void = mobx.autorun as any
+export const computed: <T>(fn: () => T) => { get(): T } = mobx.computed as any
 
-export const createTransformer: <P, R>(fn: (p: P) => R) => (p: P) => R = mobx_utils.createTransformer
+export const createTransformer: <P, R>(fn: (p: P) => R) => (p: P) => R = mobx_utils.createTransformer as any
