6b7f1a236faa5e2344e4514ca1c7d0fa80fd379e
Brandon Smith <mail@brandonsmith.ninja>
2021-11-26 16:49:02 -0600

    Fix compiler run script

--- compiler/run.sh
+++ compiler/run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-compiler_path=$( cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" ; pwd -P )
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+compiler_path="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
 deno run --allow-read --allow-write --allow-net --allow-env --allow-run --unstable "$compiler_path/index.ts" "$@"
