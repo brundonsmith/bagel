0c5fd5a88652c82f3d534ed42129b9048c5100cf
Brandon Smith <mail@brandonsmith.ninja>
2021-12-08 19:33:54 -0600

    Added a couple mobx functions to wrapper file

--- compiler/mobx.ts
+++ compiler/mobx.ts
@@ -2,9 +2,11 @@
 import * as mobx from "https://jspm.dev/npm:mobx@6.3.3"
 import * as mobx_utils from "https://jspm.dev/npm:mobx-utils@6.0.4"
 
+export const makeAutoObservable: (obj: object) => void = mobx.makeAutoObservable as any
 export const configure: (options: Record<string, unknown>) => void = mobx.configure as any
 export const observable: <T>(x: T) => T = mobx.observable as any
 export const autorun: (fn: () => void) => void = mobx.autorun as any
+export const reaction: <T>(fn: () => T, eff: (val: T) => void) => void = mobx.reaction as any
 export const computed: <T>(fn: () => T) => { get(): T } = mobx.computed as any
 
 export const computedFn: <F extends (...args: any[]) => any>(fn: F) => F = mobx_utils.computedFn as any
