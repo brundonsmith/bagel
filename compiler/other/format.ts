c78307d90c91709c837a773563b6ec6644aaff35
Brandon Smith <mail@brandonsmith.ninja>
2022-01-15 21:40:33 -0600

    Added tests for formatter, and fixed a bunch of discovered bugs

--- compiler/other/format.ts
+++ compiler/other/format.ts
@@ -23,19 +23,19 @@ function indentation(options: FormatOptions, indent: number) {
     return str
 }
 
-function formatTypeParam(param: { name: PlainIdentifier, extends: TypeExpression | undefined }, options: FormatOptions, indent: number): string {
-    const f = formatInner(options, indent)
+function formatTypeParam(param: { name: PlainIdentifier, extends: TypeExpression | undefined }, options: FormatOptions, indent: number, parent: AST|undefined): string {
+    const f = formatInner(options, indent, parent)
 
     return param.name.name + (param.extends ? ' extends ' + f(param.extends) : '')
 }
 
 export function format(ast: AST, options: FormatOptions): string {
-    return formatInner(options, 0)(ast)
+    return formatInner(options, 0, undefined)(ast)
 }
 
-const formatInner = (options: FormatOptions, indent: number) => (ast: AST): string => {
-    const f = formatInner(options, indent)
-    const fIndent = formatInner(options, indent + 1)
+const formatInner = (options: FormatOptions, indent: number, parent: AST|undefined) => (ast: AST): string => {
+    const f = formatInner(options, indent, ast)
+    const fIndent = formatInner(options, indent + 1, ast)
     const currentIndentation = indentation(options, indent)
     const nextIndentation = indentation(options, indent + 1)
 
@@ -49,10 +49,12 @@ const formatInner = (options: FormatOptions, indent: number) => (ast: AST): stri
         case "func-declaration":
         case "store-function":
             return (ast.kind === 'func-declaration' && ast.exported ? 'export ' : '') + 
+                (ast.kind === 'store-function' && ast.access ? ast.access + ' ' : '') + 
                 `func ${ast.memo ? 'memo ' : ''}${ast.name.name}${f(ast.value)}`
         case "proc-declaration":
         case "store-procedure":
             return (ast.kind === 'proc-declaration' && ast.exported ? 'export ' : '') + 
+                (ast.kind === 'store-procedure' && ast.access ? ast.access + ' ' : '') + 
                 `proc ${ast.name.name}${f(ast.value)}`
         case "arg":
             return ast.name.name + (ast.type ? ': ' + f(ast.type) : '')
@@ -67,7 +69,7 @@ const formatInner = (options: FormatOptions, indent: number) => (ast: AST): stri
         case "binary-operator":
             return `${f(ast.base)} ${ast.ops.map(([op, expr]) => f(op) + ' ' + f(expr)).join(' ')}`
         case "negation-operator":
-            return `!${ast.base}`
+            return `!${f(ast.base)}`
         case "operator":
             return ast.op
         // TODO: Put obj and array literals all on one line vs indented based on size of contents
@@ -76,16 +78,12 @@ const formatInner = (options: FormatOptions, indent: number) => (ast: AST): stri
                     nextIndentation +
                     (Array.isArray(entry) 
                         ? `${entry[0].name}: ${fIndent(entry[1])}`
-                        : '...' + f(entry as Spread))
+                        : f(entry as Spread))
                 ).join(',\n')
             }\n${currentIndentation}}`;
         case "array-literal":
             return `[${
-                ast.entries.map(entry =>
-                    (entry.kind !== 'spread'
-                        ? f(entry)
-                        : '...' + f(entry))
-                ).join(', ')
+                ast.entries.map(f).join(', ')
             }]`
         case "string-literal":
             return `'${ast.segments.map(segment => typeof segment === 'string' ? segment : '${' + f(segment) + '}')}'`
@@ -97,16 +95,18 @@ const formatInner = (options: FormatOptions, indent: number) => (ast: AST): stri
         case "nil-literal":
             return 'nil'
         case "store-declaration":
-            return (ast.exported ? 'export ' : '') + `store ${ast.name.name} {${ast.members.map(m => nextIndentation + fIndent(m)).join('\n')}\n${currentIndentation}}`
+            return (ast.exported ? 'export ' : '') + `store ${ast.name.name} {\n${ast.members.map(m => nextIndentation + fIndent(m)).join('\n\n')}\n${currentIndentation}}`
         case "store-property":
-            return `${ast.access} ${ast.name.name}${ast.type ? ': ' + f(ast.type) : ''} = ${ast.value}`
+            return `${ast.access ? ast.access + ' ' : ''}${ast.name.name}${ast.type ? ': ' + f(ast.type) : ''} = ${f(ast.value)}`
         case "let-declaration":
             return   `let ${ast.name.name}${ast.type ? ': ' + f(ast.type) : ''} = ${f(ast.value)};`
         case "const-declaration-statement":
             return `const ${ast.name.name}${ast.type ? ': ' + f(ast.type) : ''} = ${f(ast.value)};`
-        case "if-else-statement":
         case "if-else-expression":
-            return `if ${ast.cases.map(f).join(' else ')}`
+            return `if ${ast.cases.map(f).join(' else if ')}`
+                + (ast.defaultCase ? ` else {\n${nextIndentation}` + f(ast.defaultCase) + `\n${currentIndentation}}` : '')
+        case "if-else-statement":
+            return `if ${ast.cases.map(f).join(' else if ')}`
                 + (ast.defaultCase ? ' else ' + f(ast.defaultCase) : '')
         case "plain-identifier":
         case "local-identifier":
@@ -118,28 +118,29 @@ const formatInner = (options: FormatOptions, indent: number) => (ast: AST): stri
         case "while-loop":
             return `while ${f(ast.condition)} ${f(ast.body)}`
         case "invocation":
-            return `${f(ast.subject)}(${ast.args.map(f).join(', ')})`
+            return `${f(ast.subject)}${ast.typeArgs.length > 0 ? `<${ast.typeArgs.map(f).join(', ')}>` : ''}(${ast.args.map(f).join(', ')})` + (parent?.kind === 'block' ? ';' : '')
         case "property-accessor":
             return `${f(ast.subject)}${ast.optional ? '?' : ''}.${ast.property.name}`
         case "func": {
             const funcType = ast.type.kind === 'generic-type' ? ast.type.inner : ast.type
-            return `${ast.type.kind === 'generic-type' ? '<' + ast.type.typeParams.map(p => formatTypeParam(p, options, indent)).join(', ') + '>' : ''}(${funcType.args.map(f).join(', ')})${funcType.returnType ? f(funcType.returnType) : ''} => ${f(ast.body)}`
+            return `${ast.type.kind === 'generic-type' ? '<' + ast.type.typeParams.map(p => formatTypeParam(p, options, indent, parent)).join(', ') + '>' : ''}(${funcType.args.map(f).join(', ')})${funcType.returnType ? ': ' + f(funcType.returnType) : ''} =>\n${nextIndentation}${fIndent(ast.body)}`
         }
         case "proc": {
             const procType = ast.type.kind === 'generic-type' ? ast.type.inner : ast.type
-            return `${ast.type.kind === 'generic-type' ? '<' + ast.type.typeParams.map(p => formatTypeParam(p, options, indent)).join(', ') + '>' : ''}(${procType.args.map(f).join(', ')}) ${f(ast.body)}`
+            return `${ast.type.kind === 'generic-type' ? '<' + ast.type.typeParams.map(p => formatTypeParam(p, options, indent, parent)).join(', ') + '>' : ''}(${procType.args.map(f).join(', ')}) ${f(ast.body)}`
         }
         case "as-cast":
             return `${f(ast.inner)} as ${f(ast.type)}`
         case "range":
             return `${ast.start}..${ast.end}`
         case "spread":
-            return `...${ast.expr}`
+            return `...${f(ast.expr)}`
         case "indexer":
             return `${f(ast.subject)}[${f(ast.indexer)}]`
         case "autorun-declaration":
             return `autorun ${f(ast.effect)}`
         case "case":
+            return `${f(ast.condition)} {\n${nextIndentation}${fIndent(ast.outcome)}\n${currentIndentation}}`
         case "case-block":
             return `${f(ast.condition)} ${f(ast.outcome)}`
         case "switch-case":
@@ -156,13 +157,15 @@ const formatInner = (options: FormatOptions, indent: number) => (ast: AST): stri
             return `const ${ast.name.name}${ast.type ? ': ' + f(ast.type) : ''} = ${f(ast.value)},\n${currentIndentation}${f(ast.next)}`
         case "element-tag":
             return `<${ast.tagName.name}${ast.attributes.length > 0 ? ' ' + ast.attributes.map(([name, value]) => `${name.name}={${f(value)}}`).join(' ') : ''}>${
-                ast.children.map(c => '\n' + nextIndentation + fIndent(c)).join('')
+                ast.children.map(c =>
+                    '\n' + nextIndentation + (c.kind === 'element-tag' ? fIndent(c) : `{${fIndent(c)}}`)
+                ).join('')
             }${ast.children.length > 0 ? '\n' + currentIndentation : ''}</${ast.tagName.name}>`
         case "pipe":
             return `${f(ast.args[0])} |> ${f(ast.subject)}`
         case "javascript-escape":
             return `js#${ast.js}#js`
-        case "union-type": return '(' + ast.members.map(f).join(" | ") + ')';
+        case "union-type": return ast.members.map(f).join(" | ");
         case "maybe-type": return f(ast.inner) + '?';
         case "named-type":
         case "generic-param-type": return ast.name.name;
@@ -170,7 +173,7 @@ const formatInner = (options: FormatOptions, indent: number) => (ast: AST): stri
         case "bound-generic-type": return `${f(ast.generic)}<${ast.typeArgs.map(f).join(',')}>`;
         case "proc-type": return `(${ast.args.map(arg => arg.name.name + (arg.type ? `: ${f(arg.type)}` : '')).join(', ')}) {}`;
         case "func-type": return `(${ast.args.map(arg => arg.name.name + (arg.type ? `: ${f(arg.type)}` : '')).join(', ')}) => ${f(ast.returnType ?? UNKNOWN_TYPE)}`;
-        case "object-type":  return (ast.mutability !== 'mutable' ? 'const ' : '') + `{${ast.spreads.map(s => '...' + f(s)).concat(ast.entries.map(f)).join(', ')}}`;
+        case "object-type":  return (ast.mutability !== 'mutable' ? 'const ' : '') + `{${ast.spreads.map(s => nextIndentation + '...' + fIndent(s)).concat(ast.entries.map(e => nextIndentation + fIndent(e))).join(', ')}}`;
         case "attribute": return  `${ast.name.name}: ${f(ast.type)}`
         case "indexer-type": return (ast.mutability !== 'mutable' ? 'const ' : '') + `{ [${f(ast.keyType)}]: ${f(ast.valueType)} }`;
         case "array-type":   return (ast.mutability !== 'mutable' ? 'const ' : '') + `${f(ast.element)}[]`;
---
+++ tests/format_test.ts
@@ -0,0 +1,425 @@
+import { DEFAULT_OPTIONS, format } from "../compiler/other/format.ts";
+import Store from "../compiler/store.ts";
+import { deepEquals } from "../compiler/utils/misc.ts";
+import { ModuleName } from "../compiler/_model/common.ts";
+
+const BAGEL_SNIPPETS = [
+    `func uid() => '12345'`,
+    `const fn = a => a`,
+    `func memo uid() => '12345'`,
+    `const x = a < b`,
+    `func uid(n: number) => 
+        const double = 2 * n,
+        const ten = 5 * double,
+        ten`,
+    `const x = a.b.c`,
+    `const x = a.b()`,
+    `const x = a.b()()`,
+    `const x = a.b.c()`,
+    `const x = a.b().c()`,
+    `const x = a
+        .b
+        .c`,
+    `const x = a
+        .b()
+        .c()`,
+    `func merge() =>
+            if arr1.length <= 0 {
+                2
+            } else {
+                3
+            }`,
+    `func merge() =>
+            if arr1.length <= 0 {
+                2
+            } else if arr1.length <= 1 {
+                3
+            } else {
+              4
+            }`,
+    `proc foo() {
+            }`,
+    `proc foo() {
+              if true {
+                log('true');
+              } else if false {
+                log('false');
+              } else {
+                log('other');
+              }
+            }`,
+    `
+      const a = { foo: 'stuff' }
+      const b = { ...a, bar: 'other' }`,
+    `
+      const a = [1, 2, 3]
+      const b = [...a, 4]`,
+    `func uid(arr, i) => arr[i]`,
+    `const x = [ arr1[0] ]`,
+    `proc doStuff(a) { }`,
+    `proc doStuff(items: Iterator<number>) {
+            let count = 0;
+            
+            for item of items {
+            }
+
+            console.log(count);
+        }`,
+    `proc doStuff(items: Iterator<number>) {
+            let count = 0;
+
+            for item of items {
+                if item.foo {
+                    count = count + 1;
+                }
+
+                if count > 12 {
+                    console.log(items);
+                } else {
+                    console.log(nil);
+                }
+            }
+
+            console.log(count);
+        }`,
+    `const foo: FooType = 'stuff'`,
+    `const foo = bar.prop1.prop2`,
+    `func classNames(cn) =>
+                cn
+                    |> entries 
+                    |> fromEntries`,
+    `func myFunc(a, b) => 
+                0..10 
+                |> map((n) => n * 2) 
+                |> filter((n) => n < 10)`,
+    `func foo(a: string, b: number): number => 0`,
+    `proc bar(a: string[], b: { foo: number }) { }`,
+    `export type MyFn = (a: number, b: string) => string[]`,
+    `type Foo = string | number[]`,
+    `type Foo = (a: string) => (b: number|boolean) => { foo: nil[] }`,
+    `func foo() => 13 // foo bar comment
+            const a = 12`,
+    `func foo() => 13 /* foo bar comment
+            moar comment*/
+            const a = 12`,
+    `
+    const a: boolean = true
+    const b: boolean = true
+
+    const foo = !a && b
+    `,
+    `const x: string = 'foo'`, `const x: number = 'foo'`, `const x: boolean = 2 + 2 == 4`, `const x: string = 2 + 'foo' + 'bar'`, `const x: string = 2 * 3 + 'foo' + 'bar'`, `const x: number = 2 * 3 / 12`, `
+        func foo(fn: (val: number) => boolean) => nil
+        const x = foo(n => n)`, `
+        func foo(fn: (val: number) => boolean) => nil
+        const x = foo(n => n > 0)`, `
+        func foo(fn: (val: number) => boolean) => nil
+        const x = foo((n: number) => n > 0)`, `
+        func foo(fn: (val: number) => boolean) => nil
+        const x = foo((n: string) => n == 'foo')`, `const x = 'foo'\nconst y: number = x`, `func fn(_: string): string => 'foo'`, `func fn(): number => 'foo'`, `
+        func fn(_: string) => 'foo'
+        const y: string = fn('z')`, `
+        func fn(_: string) => 'foo'
+        const y: number = fn('z')`, `
+        const a = { foo: 'stuff' }
+        const b: { foo: string, bar: string } = { ...a, bar: 'other' }`, `
+        const a = { foo: 'stuff' }
+        const b: { foo: number, bar: string } = { ...a, bar: 'other' }`, `
+        const a = [1, 2, 3]
+        const b: number[] = [...a, 4]`, `
+        const a = [1, 2, 3]
+        const b: number = [...a, '4'][2]`, `
+        func getThird<T extends string[]>(arr: T) => arr[2]
+        const third: string|nil = getThird(['one', 'two', 'three'])`, `
+        const a = 12
+        const b = [...a, 4]`, `
+        const a = ['1', '2', '3']
+        const b: number[] = [...a, 4]`, `type MyObj = {
+              foo: string,
+              bar: {
+                  other: number|nil
+              }
+          }
+          
+          const obj: MyObj = {
+              foo: 'stuff',
+              bar: {
+                  other: 12
+              }
+          }`, `type MyObj = {
+              foo: string,
+              bar: {
+                  other: number|nil
+              }
+          }
+          
+          const obj: MyObj = {
+              foo: 'stuff',
+              bar: {
+                  other: 'foo'
+              }
+          }`, `
+        func foo(fn: (n: number) => number) => fn(12)
+        const bar = foo(n => 2 * n)`, `
+        func other<T>(a: T): T => a
+        const c: number = other<number>(12)`, `
+        func other<T>(a: T): T => a
+        const c: number = other<number>('foo')`, `
+        func other<T extends { foo: number }>(a: T): number => a.foo
+        const c: number = other<{ foo: number, bar: string }>({ foo: 12, bar: 'stuff' })`, `
+        func other<T extends { foo: number }>(a: T): number => a.foo
+        const c: number = other<{ foo: string, bar: string }>({ foo: 'stuff', bar: 13 })`, `
+        func other<T extends { foo: number }>(a: T): number => a
+        const c: number = other<{ foo: number, bar: string }>({ foo: 12, bar: 'stuff' })`, `
+        func other<T>(a: T): T => a
+        const c: string = other<number>(12)`, `
+        func other<T>(a: T) => a
+        const c: number = other<number>(12)`, `
+        func fnA<R>(a: R) => a
+        func fnB<T>(b: T): T => fnA<T>(b)
+        const c: number = fnB<number>(12)`, `
+        func fnA<T>(a: R) => a
+        func fnB<T>(b: T): T => fnA<T>(12)
+        const c: number = fnB<number>(12)`, `
+        func fnA<R>(a: R): R => a
+        func fnB<T>(b: T) => fnA<T>(b)
+        const c: number = fnB<number>(12)`, `
+        func fnA<T>(a: R): R => a
+        func fnB<T>(b: T) => fnA<T>(b)
+        const c: string = fnB<number>(12)`, `
+        func fnA<T>(a: T): T => a
+        func fnB<T>(b: T): T => fnA<T>(b)
+        const c: number = fnB<number>(12)`, `
+        func other<T>(a: T): T => a
+        const c: number = other(12)`, `
+        func other<T>(a: T): T => a
+        const c: number = other('foo')`, `
+        func other<T>(a: T|nil): T|nil => a
+        const c: number|nil = other(12)`, `
+        func other<T>(a: T|nil): T|nil => a
+        const c: number = other(12)`, `
+        func foo<T>(val: { prop: T }) => [val.prop]
+        const x: number[] = foo<number>({ prop: 12 })`, `
+        func foo<T>(val: { prop: T }) => [val.prop]
+        const x: string[] = foo<number>({ prop: 12 })`,
+    `func foo(_: number) =>
+          const a = b + 2,
+          const b = 12,
+          2 * a`, `
+        func foo(_: number) =>
+          const b = 12,
+          const a = b + 2,
+          2 * a`, `
+        const a = b + 2
+        const b = 12`, `
+        const b = 12
+        const a = b + 2`, `
+        proc foo() {
+          let a = b;
+          let b = 12;
+        }`, `
+        proc foo() {
+          let b = 12;
+          let a = b;
+        }`, `
+        const a: number|nil = 12
+        const b = if a != nil { a + 12 }`, `
+        const a: number|nil = 12
+        const b = a + 12`, `
+        func foo(x: { bar: number|nil }): number|nil =>
+          if x.bar != nil {
+            x.bar - 12
+          }`, `
+        func foo(x: { bar: number|nil }): number|nil =>
+          x.bar - 12`, `
+        type Base = {
+          foo: number
+        }
+        
+        type Other = {
+          ...Base,
+          bar: string
+        }
+        
+        const thing: Other = {
+          foo: 12,
+          bar: 'fgsdg'
+        }`, `
+        type Base = {
+          foo: number
+        }
+        
+        type Other = {
+          ...Base,
+          bar: string
+        }
+        
+        const thing: Other = {
+          bar: 'fgsdg'
+        }`, `
+        const obj = {
+          foo: {
+            bar: 12
+          }
+        }
+  
+        const val: number = obj.foo.bar
+        `, `
+        const obj = {
+          foo: {
+            bar: 12
+          }
+        }
+  
+        const val: number = obj.foo.other
+        `, `
+        type Obj = {
+          foo: {
+            bar: number
+          }
+        }
+  
+        func fn(obj: Obj): number =>
+          obj.foo.bar
+        `, `
+        type Obj = {
+          foo: nil | {
+            bar: number
+          }
+        }
+        
+        func fn(obj: Obj): number|nil =>
+          obj.foo?.bar`, `
+        type Obj = {
+          foo: nil | {
+            bar: number
+          }
+        }
+        
+        func fn(obj: Obj): number|nil =>
+          obj.foo.bar`,
+    `
+      store Foo {
+  
+        prop: number = 12
+  
+        proc bar() {
+          this.prop = 14;
+        }
+      }
+      `, `
+      store Foo {
+  
+        prop: number = 12
+  
+        proc bar() {
+          this.prop23 = 14;
+        }
+      }
+      `, `
+          
+      store Counter {
+        count: number = 0
+  
+        public func memo render() =>
+            <div>
+                <button onClick={this.decrement}>{'-'}</button>
+                <span>{this.count}</span>
+                <button onClick={this.increment}>{'+'}</button>
+            </div>
+  
+        proc decrement() {
+            this.count = this.count - 1;
+        }
+  
+        proc increment() {
+            this.count = this.count + 1;
+        }
+      }
+      `, `
+          
+      store Counter {
+        count: number = 0
+      }
+  
+      proc bar(val: unknown) { }
+  
+      proc foo() {
+        bar(this);
+      }
+      `,
+    `
+        const foo: 'bar' = 'bar'`, `
+      func foo(val: number): number|string => val as number|string`, `
+      func foo(val: number|string): number => val as number`, `
+      const obj = { foo: 'stuff' }
+  
+      proc foo() {
+        obj.foo = 'other';
+      }`, `
+      proc foo(param: { foo: string }) {
+        param = { foo: 'stuff' };
+      }`, `
+      proc foo(param: { foo: string }) {
+        param.foo = 'stuff';
+      }`, `
+      proc foo(param: const { foo: string }) {
+        param.foo = 'stuff';
+      }`, `
+      proc foo(param: const { foo: { bar: string } }) {
+        param.foo.bar = 'stuff';
+      }`, `
+      const obj = { foo: 'bar' }
+  
+      proc foo(param: { foo: string }) {
+        let alias = obj;
+        alias.foo = 'other';
+      }`, `
+      const obj = { foo: 'bar' }
+  
+      proc foo(param: { foo: string }) {
+        let alias = obj as const { foo: string };
+        alias = { foo: 'other' };
+      }`, `
+      proc foo(param: { foo: string }) {
+        const obj = param;
+      }`, `
+      proc foo(param: { foo: string }) {
+        const obj = param;
+        obj.foo = 'other';
+      }`, `
+      const foo: (string|number)[] = ['foo', 12, 14, 'bar']`, `
+      const foo: (string|number)[] = ['foo', 12, true, 'bar']`, `
+      func foo(a: number?, b: string?, c: boolean): number|string|boolean => a ?? b ?? c`, `
+      func foo(a: string?, b: string?): string => a ?? b ?? 12`,
+]
+
+Store.start({
+    mode: "mock",
+    modules: Object.fromEntries(BAGEL_SNIPPETS.map((bgl, index) => ['snippet ' + index, bgl])),
+    watch: undefined
+})
+
+for (let i = 0; i < BAGEL_SNIPPETS.length; i++) {
+    Deno.test({
+        name: 'snippet ' + i,
+        fn() {
+            const { ast, errors } = Store.parsed(('snippet ' + i) as ModuleName, false) ?? {}
+        
+            if (ast && errors && errors.length === 0) {
+                const formattedModuleName = ('formatted snippet ' + i) as ModuleName
+                const formatted = format(ast, DEFAULT_OPTIONS)
+
+                Store.setSource(formattedModuleName, formatted)
+
+                const reParsed = Store.parsed(formattedModuleName, false)?.ast
+
+                if (!deepEquals(ast, reParsed, ["module", "code", "startIndex", "endIndex"])) {
+                    throw `Reformatted AST did not match original:\noriginal:\n${BAGEL_SNIPPETS[i]}\nformatted:\n${formatted}`
+                }
+            } else {
+                throw `Failed to parse`
+            }
+        }
+    })
+}
