a2d2e93f9bf9440278ed9c7e54562db4aa4f369d
Brandon Smith <mail@brandonsmith.ninja>
2022-01-30 14:10:22 -0600

    Another lib fix

--- lib/src/http.ts
+++ lib/src/http.ts
@@ -1,21 +1,21 @@
-import { Plan, plan } from "./core.ts";
+import { Plan } from "./core.ts";
 
 export function get(url: Parameters<typeof fetch>[0], options?: Parameters<typeof fetch>[1]): Plan<Response> {
-    return plan(() => fetch(url, { ...options, method: 'GET' }))
+    return () => fetch(url, { ...options, method: 'GET' })
 }
 
 export function post(url: Parameters<typeof fetch>[0], options?: Parameters<typeof fetch>[1]): Plan<Response> {
-    return plan(() => fetch(url, { ...options, method: 'POST' }))
+    return () => fetch(url, { ...options, method: 'POST' })
 }
 
 export function put(url: Parameters<typeof fetch>[0], options?: Parameters<typeof fetch>[1]): Plan<Response> {
-    return plan(() => fetch(url, { ...options, method: 'PUT' }))
+    return () => fetch(url, { ...options, method: 'PUT' })
 }
 
 export function del(url: Parameters<typeof fetch>[0], options?: Parameters<typeof fetch>[1]): Plan<Response> {
-    return plan(() => fetch(url, { ...options, method: 'DELETE' }))
+    return () => fetch(url, { ...options, method: 'DELETE' })
 }
 
 export function options(url: Parameters<typeof fetch>[0], options?: Parameters<typeof fetch>[1]): Plan<Response> {
-    return plan(() => fetch(url, { ...options, method: 'OPTIONS' }))
+    return () => fetch(url, { ...options, method: 'OPTIONS' })
 }
