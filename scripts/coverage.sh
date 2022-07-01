4401086e80e233cb34e85e55bf5937e9c63ce9fc
Brandon Smith <mail@brandonsmith.ninja>
2022-07-01 13:40:49 -0500

    Coverage script fix

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -9,5 +9,5 @@ DEV_MODE=true deno test --coverage=coverage --unstable --allow-all
 deno coverage coverage --lcov > coverage/cov.xml
 
 # visual summary
-deno coverage coverage | grep "%" > ./coverage/summary.txt
+deno coverage coverage | grep "[0-9]%" > ./coverage/summary.txt
 deno run --allow-all --unstable ./scripts/total_coverage.ts
